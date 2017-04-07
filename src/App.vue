6aa287092ff6778e4c8875426857d5d4e420b10e
mayanxiao <yutouleetown@gmail.com>
2017-04-07 12:29:00 +0800

    Add devDependencies

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -21,3 +8,14 @@ export default {
   margin-top: 60px;
 }
 </style>
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
