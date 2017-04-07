21a5c3642760e828e60cf521c5cb6d0913653d68
mayanxiao <yutouleetown@gmail.com>
2017-04-07 13:38:04 +0800

    Add router

---
+++ src/views/ForemanDetail.vue
@@ -0,0 +1,37 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
+<template>
+  <div class="foreman-detail">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForemanDetail',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
