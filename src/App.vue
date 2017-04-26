114bbaeaaf74d4fe368c326887a14e638967e365
mayanxiao <yutouleetown@gmail.com>
2017-04-26 15:13:30 +0800

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,24 @@
 *{
   padding: 0;
   margin: 0;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  /* color: #2c3e50; */
 }
 .mainbody {
-  min-width: 1200px;
-  width: 62.5%;
+  min-width: 1220px;
+  width: 62.6%;
   margin: 0 auto;
 }
 </style>
 <template>
   <div id="app">
-    <!-- <j-header></j-header> -->
+    <nav-bar></nav-bar>
     <div class="mainbody">
       <router-view></router-view>
     </div>
@@ -27,12 +28,14 @@
 </template>
 
 <script>
+import NavBar from '@/components/NavBar'
 import JFooter from '@/components/JFooter'
 
 export default {
   name: 'app',
   components: {
-    JFooter
+    JFooter,
+    NavBar
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.navbar {
+  width: 100%;
+  height: 250px;
+  background-color: #ddd;
+}
+</style>
+
+<template>
+  <div class="navbar">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
