c160be9f183f61ffa7b5e23f34ffac4a51a813be
xiaoheigcy <gechunyang09@163.com>
2017-05-16 13:30:05 +0800

    index1-enroll

---
+++ src/views/Index1.vue
@@ -0,0 +1,32 @@
+<style lang="less" scoped>
+	.index1{
+		padding: 20px;
+	}
+	.banner-bac{
+		height: 364px;
+	}
+</style>
+
+<template>
+	<div class="index1">
+		<div class="banner-bac">
+			<Enroll></Enroll>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	import Enroll from "../components/Enroll"
+	export default {
+		name: 'impression',
+		components: {
+			Enroll
+		},
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
